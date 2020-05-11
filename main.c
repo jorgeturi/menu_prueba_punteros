b0db82a5885f45c1a58228a37957a06cfc8d94a9
jorge <63058024+jorgeturi@users.noreply.github.com>
2020-05-11 10:28:49 -0300

    cambios

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 void borrar_pantalla(void);
 void tecla_para_continuar(void);
 void imprimir_matriz(float **, int, int);
-void transponer_matriz(float **, float **, int);
+void transponer_matriz(float **, float **, int, int);
 void llenar_matriz(float **, int, int);
 int comprobar_reserva_matriz(float **, int, int); //devuelve int por si algun espacio no puede ser agarrado
 void reserva_memoria_para_matriz(float **, int, int);
@@ -14,8 +14,8 @@ void reserva_memoria_para_matriz(float **, int, int);
 
 int main()
 {
-    int N_COLUMNAS = 4;
-    int N_FILAS = 4;
+    int N_COLUMNAS = 2;
+    int N_FILAS = 2;
     int matriz_cuadrada = 0;
     int ORDEN = 0;
 
@@ -32,17 +32,9 @@ int main()
         ORDEN = N_FILAS;
     }
 
-   
 
-    
-   
 
- 
-    
 
-    
-
-    
     /*fflush(stdin);
     char ingresado;
     int a;
@@ -51,13 +43,6 @@ int main()
        a = isdigit (ingresado) ;
        printf ("%d" , a);
        */
-
-
-
-
-
-
-
 
 
 
@@ -81,57 +66,53 @@ int main()
     printf("\nElija su opcion: \n\t\t");        //se elije una opcion y se guarda en opcion_elegida
     scanf("%c",&opcion_elegida);
 
+
     switch (opcion_elegida)                //segun se haya tocado, pasan los casos
 {
 
     case '1':
-        borrar_pantalla();                
+        borrar_pantalla();
         //reserva de memoria manual hasta que la funcion funcione
-         //reserva_memoria_para_matriz(matriz,N_COLUMNAS,N_FILAS);
-
-         matriz=(float**)malloc(N_COLUMNAS*sizeof(float*)); //reservo a matriz "numero de columas" columnas en heap
-         for (int i=0 ; i<= N_FILAS ; i++)                               //reservo a matriz "numero de filas" filas en heap
-         {
-         *(matriz+i)=(float*)malloc(N_FILAS*sizeof(float));
-         }
+        matriz=(float**)malloc(N_COLUMNAS*sizeof(float*));//no se por que esto tiene que ir antes
+        reserva_memoria_para_matriz(matriz,N_COLUMNAS,N_FILAS);
 
         comprobar_reserva_matriz(matriz, N_COLUMNAS, N_FILAS);
         llenar_matriz(matriz,N_COLUMNAS,N_FILAS);
         tecla_para_continuar();         //se espera una tecla para continuar
         borrar_pantalla();                //se borra la pantalla
-        break;                          
+        break;
 
-    case '2':  
+    case '2':
         imprimir_matriz(matriz,N_COLUMNAS,N_FILAS);
         tecla_para_continuar();
         borrar_pantalla();
         break;
 
-    case '3': 
+    case '3':
         //reserva de memoria manual para la transpuesta
         matrizt=(float**)malloc(N_COLUMNAST*sizeof(float*)); //reservo a matriz "numero de columas" columnas en heap
-        for (int i=0 ; i<= N_FILAST ; i++)                               //reservo a matriz "numero de filas" filas en heap
-        {
-        *(matrizt+i)=(float*)malloc(N_FILAST*sizeof(float));
-        }
+        reserva_memoria_para_matriz(matrizt,N_COLUMNAST,N_FILAST);
         comprobar_reserva_matriz(matrizt, N_COLUMNAST, N_FILAST);
-
-    
-
-         transponer_matriz(matriz, matrizt, ORDEN);
-         printf ("\n \n");
-         imprimir_matriz(matrizt, N_FILAS, N_COLUMNAS);
-         printf ("\n");
+         transponer_matriz(matriz, matrizt, N_COLUMNAS, N_FILAS);
+         imprimir_matriz(matrizt, N_COLUMNAST, N_FILAST);
         tecla_para_continuar();
         borrar_pantalla();
         break;
 
-    case '4': 
+    case '4':
+        if (matriz_cuadrada == 0)
+        {
+            printf("\n no es posible calcular su adjunta \n");
+        }
         tecla_para_continuar();
         borrar_pantalla();
         break;
-    
+
     case '5':
+        if (matriz_cuadrada == 0)
+        {
+            printf("\n no es posible calcular su determinante \n");
+        }
         tecla_para_continuar();
         borrar_pantalla();
         break;
@@ -139,8 +120,7 @@ int main()
     case '6':
          if (matriz_cuadrada == 0)
        {
-        printf("no puede calcularse la inversa \n");
-        printf("no puede calcularse su determinante \n");
+        printf("\n no es posible calcular su inversa \n");
        }
         tecla_para_continuar();
         borrar_pantalla();
