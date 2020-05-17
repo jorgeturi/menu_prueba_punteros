036779c7b867a19ca41f97978ac083ba76602977
jorge <63058024+jorgeturi@users.noreply.github.com>
2020-05-17 11:31:26 -0300

    rework

--- main.c
+++ main.c
@@ -1,156 +1,146 @@
 #include <stdio.h>
-#include <malloc.h>
-#include <string.h>
 #include <stdlib.h>
 
-void borrar_pantalla(void);
-void tecla_para_continuar(void);
-void imprimir_matriz(float **, int, int);
-void transponer_matriz(float **, float **, int, int);
-void llenar_matriz(float **, int, int);
-int comprobar_reserva_matriz(float **, int, int); //devuelve int por si algun espacio no puede ser agarrado
-void reserva_memoria_para_matriz(float **, int, int);
+void imprimir_matriz(float ***, int N_FILAS , int N_COLUMNAS);
+float ** reservar_memoria(float ***matriz, int N_FILAS, int N_COLUMNAS);
+float ** llenar_matriz(float ***matriz, int N_FILAS, int N_COLUMNAS);
+float ** transponer_2_matrices(float ***matriz_a_transponer, float***matriz_transpuesta, int N_FILAS, int N_COLUMNAS);
 
+double calcular_determinante(float **fuente);
 
 int main()
 {
-    int N_COLUMNAS = 2;
-    int N_FILAS = 2;
+    float ** matriz = NULL;
+    float ** matrizt = NULL;
+
+    int N_FILAS = 3;
+    int N_COLUMNAS = 3;
+    int N_FILAST;
+    int N_COLUMNAST;
+
     int matriz_cuadrada = 0;
     int ORDEN = 0;
 
-    float**matriz; //matriz es un puntero doble
-
-    int N_COLUMNAST = N_FILAS;
-    int N_FILAST = N_COLUMNAS;
-
-    float**matrizt; //matriz es un puntero doble
-
     if (N_FILAS == N_COLUMNAS)
     {
-        matriz_cuadrada=1;
+        matriz_cuadrada = 1;
         ORDEN = N_FILAS;
     }
 
+    N_FILAST = N_COLUMNAS;
+    N_COLUMNAST = N_FILAS;
+
+    matriz = reservar_memoria(&matriz, N_FILAS, N_COLUMNAS);
+    matrizt = reservar_memoria(&matrizt, N_FILAST, N_COLUMNAST);
+
+    matriz = llenar_matriz(&matriz, N_FILAS, N_COLUMNAS);
+    matrizt = transponer_2_matrices (&matriz, &matrizt, N_FILAS, N_COLUMNAS);
 
 
+    imprimir_matriz (&matriz, N_FILAS, N_COLUMNAS);
+    imprimir_matriz (&matrizt, N_FILAST, N_COLUMNAST);
+    double determinante;
+    determinante = calcular_determinante(matriz);
+    printf ("el determinante es: %f", determinante);
 
-    /*fflush(stdin);
-    char ingresado;
-    int a;
-       scanf ("%c", &ingresado);
-       int isdigit (int);
-       a = isdigit (ingresado) ;
-       printf ("%d" , a);
-       */
-
-
-
-
-
-
-
-    char opcion_elegida = '1'; //variable para las opciones del menu
-    char confirmacion;       //variable para almacenar la confirmacion de salida
-
-    while (opcion_elegida!=0)             //mientras la opcion elegida sea distinto de 5 imprime el menu, arranca en 0 por eso al menos 1 vez se muestra
-{
-    printf("Menu de operaciones\n");      //menu
-    printf("1.Ingresar matriz\n");
-    printf("2.Imprimir matriz\n");
-    printf("3.Transponer matriz\n");
-    printf("4.Calcular matriz adjunta\n");
-    printf("5.Calcular determinante\n");
-    printf("6.Calcular inversa, si existe\n");
-    printf("0.Salir\n");
-    printf("\nElija su opcion: \n\t\t");        //se elije una opcion y se guarda en opcion_elegida
-    scanf("%c",&opcion_elegida);
-
-
-    switch (opcion_elegida)                //segun se haya tocado, pasan los casos
-{
-
-    case '1':
-        borrar_pantalla();
-        //reserva de memoria manual hasta que la funcion funcione
-        matriz=(float**)malloc(N_COLUMNAS*sizeof(float*));//no se por que esto tiene que ir antes
-        reserva_memoria_para_matriz(matriz,N_COLUMNAS,N_FILAS);
-
-        comprobar_reserva_matriz(matriz, N_COLUMNAS, N_FILAS);
-        llenar_matriz(matriz,N_COLUMNAS,N_FILAS);
-        tecla_para_continuar();         //se espera una tecla para continuar
-        borrar_pantalla();                //se borra la pantalla
-        break;
-
-    case '2':
-        imprimir_matriz(matriz,N_COLUMNAS,N_FILAS);
-        tecla_para_continuar();
-        borrar_pantalla();
-        break;
-
-    case '3':
-        //reserva de memoria manual para la transpuesta
-        matrizt=(float**)malloc(N_COLUMNAST*sizeof(float*)); //reservo a matriz "numero de columas" columnas en heap
-        reserva_memoria_para_matriz(matrizt,N_COLUMNAST,N_FILAST);
-        comprobar_reserva_matriz(matrizt, N_COLUMNAST, N_FILAST);
-         transponer_matriz(matriz, matrizt, N_COLUMNAS, N_FILAS);
-         imprimir_matriz(matrizt, N_COLUMNAST, N_FILAST);
-        tecla_para_continuar();
-        borrar_pantalla();
-        break;
-
-    case '4':
-        if (matriz_cuadrada == 0)
-        {
-            printf("\n no es posible calcular su adjunta \n");
-        }
-        tecla_para_continuar();
-        borrar_pantalla();
-        break;
-
-    case '5':
-        if (matriz_cuadrada == 0)
-        {
-            printf("\n no es posible calcular su determinante \n");
-        }
-        tecla_para_continuar();
-        borrar_pantalla();
-        break;
-
-    case '6':
-         if (matriz_cuadrada == 0)
-       {
-        printf("\n no es posible calcular su inversa \n");
-       }
-        tecla_para_continuar();
-        borrar_pantalla();
-        break;
-
-     case '0': //caso salir
-        printf("seguro que queres salir? (s/n) \n");
-        scanf("%c",&confirmacion);
-        scanf("%c",&confirmacion);   //no estoy seguro por que pero creo que queda algo en la consola por eso tengo que poner 2
-        if (confirmacion=='s' || confirmacion=='S') //si seguro quiere salir, retorno 0, termina el programa
-        {
-            opcion_elegida=0;
-        }
-        else
-        {
-            printf("entonces volvamos al menu\n"); //sino tocaste s es que no estas seguro entonces volvemos
-            opcion_elegida=80;   //cambio opcion_elegida a 80 porque el while se va a repetir si es distinto de 5, sino al evaluar terminaria
-            tecla_para_continuar();
-            borrar_pantalla();
-        }
-        break;
-
-     default:
-        printf("\nEsa opcion no es valida\n");
-        printf("Debe ingresar una opcion del 1 al 4 o 0 \n");
-        tecla_para_continuar();
-        borrar_pantalla();
-        break;
-} //fin del switch
-
-} //fin del while
+    //float **matrizadj = NULL;
     return 0;
 }
+
+
+float** reservar_memoria(float ***matriz,int N_FILAS,int N_COLUMNAS)
+{
+    *matriz=(float**)malloc(N_FILAS*sizeof(float**));
+
+    if (*matriz ==NULL)
+    {
+        puts("memoria insuficiente");
+        exit (500);
+    }
+
+
+    for (int i=0 ; i<N_FILAS ; i++)
+    {
+        *(*(matriz)+i) =(float*)malloc(N_COLUMNAS*sizeof(float*));
+
+        if ( *(*(matriz)+i) ==NULL)
+    {
+        puts("memoria insuficiente");
+        exit (500);
+    }
+
+
+    }
+
+    return *matriz;
+}
+
+void imprimir_matriz(float *** matriz_a_imprimir, int N_FILAS, int N_COLUMNAS)
+{
+    for(int i=0; i<N_FILAS; i++)
+    {
+        for(int j=0; j<N_COLUMNAS; j++)
+        {
+            printf("\t[%d][%d] = %3f", i, j, *(*(*(matriz_a_imprimir)+i)+j) );
+        }
+        puts("\n");
+    }
+}
+
+float ** llenar_matriz(float ***matriz,int N_FILAS,int N_COLUMNAS)
+{
+
+        for(int f=0; f<N_FILAS; f++)
+        {
+            printf("\tFila numero %d:\n",f+1);
+            for(int c=0; c<N_COLUMNAS; c++)
+            {
+                printf("\t\tColumna %d [%d][%d] = ", c, f, c);
+                scanf("%f", *(*(matriz)+f)+c );
+            }
+            printf("\n");
+        }
+
+
+    return *matriz;
+}
+
+float ** transponer_2_matrices(float ***matriz_a_transponer, float***matriz_transpuesta, int N_FILAS, int N_COLUMNAS)
+{
+  for(int i=0; i<N_FILAS; i++)
+    {
+        for(int j=0; j<N_COLUMNAS; j++)
+        {
+                *(*(*(matriz_transpuesta)+j)+i) = *(*(*(matriz_a_transponer)+i)+j);
+        }
+    }
+
+    return *matriz_transpuesta;
+}
+
+float **calcular_adjunta(float **fuente, float **destino)
+{
+    destino[0][0] = fuente[1][1]*fuente[2][2]-fuente[2][1]*fuente[1][2];
+    destino[0][1] = -(fuente[1][0]*fuente[2][2]-fuente[2][0]*fuente[1][2]);
+    destino[0][2] = fuente[1][0]*fuente[2][1]-fuente[2][0]*fuente[1][1];
+    destino[1][0] = -(fuente[0][1]*fuente[2][2]-fuente[2][1]*fuente[0][2]);
+    destino[1][1] = fuente[0][0]*fuente[2][2]-fuente[2][0]*fuente[0][2];
+    destino[1][2] = -(fuente[0][0]*fuente[2][1]-fuente[2][0]*fuente[0][1]);
+    destino[2][0] = fuente[0][1]*fuente[1][2]-fuente[1][1]*fuente[0][2];
+    destino[2][1] = -(fuente[0][0]*fuente[1][2]-fuente[1][0]*fuente[0][2]);
+    destino[2][2] = fuente[0][0]*fuente[1][1]-fuente[1][0]*fuente[0][1];
+
+    return destino;
+}
+
+double calcular_determinante(float **fuente)
+{
+    double determinante;
+
+    determinante = (fuente[0][0]*fuente[1][1]*fuente[2][2]+fuente[0][1]*fuente[1][2]*fuente[2][0]+
+                    fuente[1][0]*fuente[2][1]*fuente[0][2]-(fuente[0][2]*fuente[1][1]*fuente[2][0])-
+                    (fuente[0][1]*fuente[1][0]*fuente[2][2])-(fuente[1][2]*fuente[2][1]*fuente[0][0]));
+
+    return determinante;
+}
+
